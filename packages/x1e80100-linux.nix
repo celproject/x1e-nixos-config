db127f0d9100ea787d00709d71f7feb491443279
celproject <123776900+celproject@users.noreply.github.com>
2024-10-24 19:15:34 +0300

    Update x1e80100-linux.nix
    updated to kernel 6.12-rc4

--- packages/x1e80100-linux.nix
+++ packages/x1e80100-linux.nix
@@ -4,10 +4,10 @@ linuxPackagesFor (buildLinux {
   src = fetchFromGitHub {
     owner = "jhovold";
     repo = "linux";
-    rev = "wip/x1e80100-6.12-rc3";  # The specific branch for Lenovo Yoga Slim 7x
-    hash = "sha256-kpuzjqcI4YGS+S9OvIUhm6z8xCGMA5h5+JlcHhoEETM=";  # SHA256 hash you just retrieved
+    rev = "wip/x1e80100-6.12-rc4";
+    hash = "sha256-lW9NjL4HQzeQpPnBVpUr3m5ZKV+f8jWC77i5en9IgOo=";
   };
-  version = "6.12.0-rc3";  # Kernel version matching the branch
+  version = "6.12.0-rc4";  # Kernel version matching the branch
   defconfig = "johan_defconfig";  # Config specific to your device
 
   structuredExtraConfig = with lib.kernel; {
@@ -43,13 +43,6 @@ linuxPackagesFor (buildLinux {
         url = "https://lore.kernel.org/linux-arm-msm/20240927185345.3680-3-maccraft123mc@gmail.com/raw";
         hash = "sha256-tnpo07ZPi/3cdiY9h90rf2UgTjr9ZfR1PYRVVQJ2pUQ=";
       };
-    }
-    {
-    name = "drm_panic_qr_code_patch";
-    patch = fetchurl {
-      url = "https://patchwork.freedesktop.org/patch/618184/raw/";
-      hash = "sha256-7eEbN0FsbXO2Phb08MEznU6P15Eb15Ge6kB1is8b240";  # The hash in base32 format
-    };
     }
   ];
 })
